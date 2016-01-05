2cf5e7c1d905660dae0242eaa99938007f21dc8e
Jorge <jorgedjr21@gmail.com>
2016-01-05 01:15:25 -0200

    config paths working gulp tasks working

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,67 @@
 var elixir      = require('laravel-elixir'),
     livereload  = require('gulp-livereload'),
-    clean       = require('gulp-clean'),
+    clean       = require('rimraf'),
     gulp        = require('gulp');
 
+var config = {
+    assets_path:'./resources/assets',
+    build_path:'./public/build'
+};
 
-gulp.task('teste',function(){
-    console.log('está funcionando');
+config.bower_path     = config.assets_path+"/../bower_components";
+
+config.build_path_js  = config.build_path+"/js";
+config.build_vendor_path_js = config.build_path_js+"/vendor";
+config.vendor_path_js = [
+    config.bower_path+"/jquery/dist/jquery.min.js",
+    config.bower_path+"/bootstrap/dist/js/bootstrap.min.js",
+    config.bower_path+"/angular/angular.min.js",
+    config.bower_path+"/angular-route/angular-route.min.js",
+    config.bower_path+"/angular-resource/angular-resource.min.js",
+    config.bower_path+"/angular-animate/angular-animate.min.js",
+    config.bower_path+"/angular-messages/angular-messages.min.js",
+    config.bower_path+"/angular-bootstrap/ui-bootstrap.min.js",
+    config.bower_path+"/angular-strap/dist/modules/navbar.min.js"
+];
+
+config.build_path_css  = config.build_path+"/css";
+config.build_vendor_path_css = config.build_path_css+"/vendor";
+config.vendor_path_css = [
+    config.bower_path+"/bootstrap/dist/css/bootstrap.min.css",
+    config.bower_path+"/bootstrap/dist/css/bootstrap-theme.min.css"
+];
+
+gulp.task('copy-styles',function(){
+    gulp.src([config.assets_path+"/css/**/*.css"])
+        .pipe(gulp.dest(config.build_path_css))
+        .pipe(livereload());
+
+    gulp.src(config.vendor_path_css)
+        .pipe(gulp.dest(config.build_vendor_path_css))
+        .pipe(livereload());
+
 });
+
+gulp.task('copy-scripts',function(){
+    gulp.src([config.assets_path+"/js/**/*.js"])
+        .pipe(gulp.dest(config.build_path_js))
+        .pipe(livereload());
+
+    gulp.src(config.vendor_path_js)
+        .pipe(gulp.dest(config.build_vendor_path_js))
+        .pipe(livereload());
+});
+
+gulp.task('clear-build-folder',function(){
+    clean.sync(config.build_path);
+});
+
+gulp.task('watch-dev',['clear-build-folder'],function(){
+    livereload.listen();
+    gulp.start('copy-styles','copy-scripts');
+    gulp.watch(config.assets_path+"/**",['copy-styles','copy-scripts']);
+});
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
